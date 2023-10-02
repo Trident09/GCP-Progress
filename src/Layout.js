e40e6be9c4c9bfa8ed8445836a99539108501811
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-10-02 11:24:26 +0000

    added confetti

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
@@ -6,6 +7,13 @@ import PreLoader from "./components/PreLoader";
 
 export default function Layout() {
 	const [isLoading, setIsLoading] = useState(true);
+	const [isConfetti, setIsConfetti] = useState(true);
+	const [bodyWidth, setBodyWidth] = useState(document.body.innerWidth);
+
+	function handleSizeChange() {
+		setBodyWidth(document.body.innerWidth);
+	}
+
 	useEffect(() => {
 		const delay = () => {
 			setTimeout(() => {
@@ -14,10 +22,28 @@ export default function Layout() {
 		};
 		delay();
 	}, []);
+	useEffect(() => {
+		document.body.onresize = () => handleSizeChange();
+		const delay = () => {
+			setTimeout(() => {
+				setIsConfetti(false);
+			}, 9000);
+		};
+		delay();
+	}, []);
 	return isLoading ? (
 		<PreLoader />
 	) : (
 		<body>
+			{isConfetti && (
+				<Confetti
+					width={bodyWidth}
+					height={window.innerHeight}
+					numberOfPieces={200}
+					gravity={0.06}
+					wind={0.01}
+				/>
+			)}
 			<NavBar />
 			<main className="pt-20 min-h-[95vh]">
 				<Outlet />
