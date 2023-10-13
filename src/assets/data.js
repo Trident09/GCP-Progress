6319fd3de47dff7eeb69651208ef6990dbf0a4a2
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-10-13 12:07:07 +0530

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,36 @@
 export const Date = "9 Oct 2023, 23: 40 ( FINAL UPDATE )";
 
-export const DataCompletes = [
-    {
-        "Student Name": "No Data Found",
+export const DataCompletes = [];
+
+for(let i = 1; i <= 89; i++) {
+    DataCompletes.push({
+        "Student Name": `Dummy Name ${i}`,
+        "# of Courses Completed": "4",
+        "# of Skill Badges Completed": "4",
+        "# of GenAI Game Completed": "1",
+        "Total Completions of both Pathways": "Yes",
+        "Redemption Status": "Yes"
+    });
+}
+
+for(let i = 1; i <= 51; i++) {
+    DataCompletes.push({
+        "Student Name": `Dummy Name ${i}`,
+        "# of Courses Completed": "NA",
+        "# of Skill Badges Completed": "NA",
+        "# of GenAI Game Completed": "NA",
+        "Total Completions of both Pathways": "NA",
+        "Redemption Status": "Yes"
+    });
+}
+
+for(let i = 1; i <= 37; i++) {
+    DataCompletes.push({
+        "Student Name": `Dummy Name ${i}`,
         "# of Courses Completed": "NA",
         "# of Skill Badges Completed": "NA",
         "# of GenAI Game Completed": "NA",
         "Total Completions of both Pathways": "NA",
         "Redemption Status": "NA"
-    }
-]
+    });
+}
