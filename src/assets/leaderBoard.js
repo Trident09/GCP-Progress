9a7a50eef7e120ff8323b9cb9fbc5103018deaa2
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2024-08-10 20:37:36 +0530

    Dummy Update

--- src/assets/leaderBoard.js
+++ src/assets/leaderBoard.js
@@ -1,366 +1,366 @@
 export const LeaderBoards = [
 	{
 		sl: "1",
-		Name: "Anand Kumar",
-	},
-	{
-		sl: "2",
-		Name: "RAHUL BERA",
-	},
-	{
-		sl: "3",
-		Name: "Kartik Gautam",
-	},
-	{
-		sl: "4",
-		Name: "Jatin Sharma",
-	},
-	{
-		sl: "5",
-		Name: "Manish kumar",
-	},
-	{
-		sl: "6",
-		Name: "Kajal Gupta",
-	},
-	{
-		sl: "7",
-		Name: "Aman Gupta",
-	},
-	{
-		sl: "8",
-		Name: "Prakhar tiwari",
-	},
-	{
-		sl: "9",
-		Name: "Aditya Garg",
-	},
-	{
-		sl: "10",
-		Name: "Manotosh Sarkar",
-	},
-	{
-		sl: "11",
-		Name: "Ayushi Bhasker",
-	},
-	{
-		sl: "12",
-		Name: "Bhagyesh Mahesh Katkar",
-	},
-	{
-		sl: "13",
-		Name: "Abhishek Prajapati",
-	},
-	{
-		sl: "14",
-		Name: "Md sohel",
-	},
-	{
-		sl: "15",
-		Name: "Supriya Singh",
-	},
-	{
-		sl: "16",
-		Name: "Raj Chakraborty",
-	},
-	{
-		sl: "17",
-		Name: "Ayush Anand",
-	},
-	{
-		sl: "18",
-		Name: "Jatin Sharma",
-	},
-	{
-		sl: "19",
-		Name: "Joyita Bhawal",
-	},
-	{
-		sl: "20",
-		Name: "Sachin Narwal",
-	},
-	{
-		sl: "21",
-		Name: "Sumit Kumar",
-	},
-	{
-		sl: "22",
-		Name: "Surat Singh",
-	},
-	{
-		sl: "23",
-		Name: "Shishir Shrivastava",
-	},
-	{
-		sl: "24",
-		Name: "Abhishek Kumar",
-	},
-	{
-		sl: "25",
-		Name: "Mitali Raj",
-	},
-	{
-		sl: "26",
-		Name: "Garv Chauhan",
-	},
-	{
-		sl: "27",
-		Name: "Aviral K",
-	},
-	{
-		sl: "28",
-		Name: "Dhruv chauhan",
-	},
-	{
-		sl: "29",
-		Name: "Garv Nihore",
-	},
-	{
-		sl: "30",
-		Name: "Mansha Rohilla",
-	},
-	{
-		sl: "31",
-		Name: "Somya Bhatt",
-	},
-	{
-		sl: "32",
-		Name: "Rupam Barui",
-	},
-	{
-		sl: "33",
-		Name: "Dayananda Bindhani",
-	},
-	{
-		sl: "34",
-		Name: "Rishabh Kumar",
-	},
-	{
-		sl: "35",
-		Name: "Abhinand Anilkumar",
-	},
-	{
-		sl: "36",
-		Name: "Aniket Shahi",
-	},
-	{
-		sl: "37",
-		Name: "prashant hiremath",
-	},
-	{
-		sl: "38",
-		Name: "Divyanshu Kashyap",
-	},
-	{
-		sl: "39",
-		Name: "Akash",
-	},
-	{
-		sl: "40",
-		Name: "Abhay Gupta",
-	},
-	{
-		sl: "41",
-		Name: "Vijay hiremath",
-	},
-	{
-		sl: "42",
-		Name: "Satyam Krishna",
-	},
-	{
-		sl: "43",
-		Name: "Madhav Jadav",
-	},
-	{
-		sl: "44",
-		Name: "Aditya Bahl",
-	},
-	{
-		sl: "45",
-		Name: "Ritesh Kumar Goel",
-	},
-	{
-		sl: "46",
-		Name: "Netransh Adhoj",
-	},
-	{
-		sl: "47",
-		Name: "Anushka Ata",
-	},
-	{
-		sl: "48",
-		Name: "Rusi",
-	},
-	{
-		sl: "49",
-		Name: "Khushi Shrivastava",
-	},
-	{
-		sl: "50",
-		Name: "Shreyash",
-	},
-	{
-		sl: "51",
-		Name: "Dhruv Suri",
-	},
-	{
-		sl: "52",
-		Name: "VIVEK KUMAR",
-	},
-	{
-		sl: "53",
-		Name: "Kushagra Singh",
-	},
-	{
-		sl: "54",
-		Name: "Aditi Nagpal",
-	},
-	{
-		sl: "55",
-		Name: "Sourabh Roj",
-	},
-	{
-		sl: "56",
-		Name: "Alok Danve",
-	},
-	{
-		sl: "57",
-		Name: "Bhawana",
-	},
-	{
-		sl: "58",
-		Name: "Rakhi Kumari",
-	},
-	{
-		sl: "59",
-		Name: "Suresh",
-	},
-	{
-		sl: "60",
-		Name: "Shreya",
-	},
-	{
-		sl: "61",
-		Name: "Vivek Kumar",
-	},
-	{
-		sl: "62",
-		Name: "Tisya Kaushik",
-	},
-	{
-		sl: "63",
-		Name: "Vivek kr Jha",
-	},
-	{
-		sl: "64",
-		Name: "Aakash Kharb",
-	},
-	{
-		sl: "65",
-		Name: "Roushan Kumar Singh",
-	},
-	{
-		sl: "66",
-		Name: "Priyanshi Pawar",
-	},
-	{
-		sl: "67",
-		Name: "Vaibhav Tripathi",
-	},
-	{
-		sl: "68",
-		Name: "Susovan Pati",
-	},
-	{
-		sl: "69",
-		Name: "Vijayakumar Hiremath",
-	},
-	{
-		sl: "70",
-		Name: "Kushagra",
-	},
-	{
-		sl: "71",
-		Name: "Hemanth Prabhu",
-	},
-	{
-		sl: "72",
-		Name: "Anand Kumar",
-	},
-	{
-		sl: "73",
-		Name: "Arpita Parihar",
-	},
-	{
-		sl: "74",
-		Name: "Satvik bhagat",
-	},
-	{
-		sl: "75",
-		Name: "Aditya",
-	},
-	{
-		sl: "76",
-		Name: "Digvijay Singh",
-	},
-	{
-		sl: "77",
-		Name: "Vikash",
-	},
-	{
-		sl: "78",
-		Name: "Pradeep",
-	},
-	{
-		sl: "79",
-		Name: "Aryan Sadh",
-	},
-	{
-		sl: "80",
-		Name: "Kavi",
-	},
-	{
-		sl: "81",
-		Name: "Anand Kumar",
-	},
-	{
-		sl: "82",
-		Name: "Jaideep",
-	},
-	{
-		sl: "83",
-		Name: "PRIYANSHU JENA",
-	},
-	{
-		sl: "84",
-		Name: "Prince Kumar",
-	},
-	{
-		sl: "85",
-		Name: "VISHNULAL N",
-	},
-	{
-		sl: "86",
-		Name: "Abhilasha Kumari",
-	},
-	{
-		sl: "87",
-		Name: "Kajal Meena",
-	},
-	{
-		sl: "88",
-		Name: "Manu krishnan k",
-	},
-	{
-		sl: "89",
-		Name: "Rachna Singh",
-	},
-	{
-		sl: "90",
-		Name: "Sneha Das",
-	},
-	{
-		sl: "91",
-		Name: "Aditya Dahiya",
-	},
+		Name: "Dummy Name",
+	},
+	// {
+	// 	sl: "2",
+	// 	Name: "RAHUL BERA",
+	// },
+	// {
+	// 	sl: "3",
+	// 	Name: "Kartik Gautam",
+	// },
+	// {
+	// 	sl: "4",
+	// 	Name: "Jatin Sharma",
+	// },
+	// {
+	// 	sl: "5",
+	// 	Name: "Manish kumar",
+	// },
+	// {
+	// 	sl: "6",
+	// 	Name: "Kajal Gupta",
+	// },
+	// {
+	// 	sl: "7",
+	// 	Name: "Aman Gupta",
+	// },
+	// {
+	// 	sl: "8",
+	// 	Name: "Prakhar tiwari",
+	// },
+	// {
+	// 	sl: "9",
+	// 	Name: "Aditya Garg",
+	// },
+	// {
+	// 	sl: "10",
+	// 	Name: "Manotosh Sarkar",
+	// },
+	// {
+	// 	sl: "11",
+	// 	Name: "Ayushi Bhasker",
+	// },
+	// {
+	// 	sl: "12",
+	// 	Name: "Bhagyesh Mahesh Katkar",
+	// },
+	// {
+	// 	sl: "13",
+	// 	Name: "Abhishek Prajapati",
+	// },
+	// {
+	// 	sl: "14",
+	// 	Name: "Md sohel",
+	// },
+	// {
+	// 	sl: "15",
+	// 	Name: "Supriya Singh",
+	// },
+	// {
+	// 	sl: "16",
+	// 	Name: "Raj Chakraborty",
+	// },
+	// {
+	// 	sl: "17",
+	// 	Name: "Ayush Anand",
+	// },
+	// {
+	// 	sl: "18",
+	// 	Name: "Jatin Sharma",
+	// },
+	// {
+	// 	sl: "19",
+	// 	Name: "Joyita Bhawal",
+	// },
+	// {
+	// 	sl: "20",
+	// 	Name: "Sachin Narwal",
+	// },
+	// {
+	// 	sl: "21",
+	// 	Name: "Sumit Kumar",
+	// },
+	// {
+	// 	sl: "22",
+	// 	Name: "Surat Singh",
+	// },
+	// {
+	// 	sl: "23",
+	// 	Name: "Shishir Shrivastava",
+	// },
+	// {
+	// 	sl: "24",
+	// 	Name: "Abhishek Kumar",
+	// },
+	// {
+	// 	sl: "25",
+	// 	Name: "Mitali Raj",
+	// },
+	// {
+	// 	sl: "26",
+	// 	Name: "Garv Chauhan",
+	// },
+	// {
+	// 	sl: "27",
+	// 	Name: "Aviral K",
+	// },
+	// {
+	// 	sl: "28",
+	// 	Name: "Dhruv chauhan",
+	// },
+	// {
+	// 	sl: "29",
+	// 	Name: "Garv Nihore",
+	// },
+	// {
+	// 	sl: "30",
+	// 	Name: "Mansha Rohilla",
+	// },
+	// {
+	// 	sl: "31",
+	// 	Name: "Somya Bhatt",
+	// },
+	// {
+	// 	sl: "32",
+	// 	Name: "Rupam Barui",
+	// },
+	// {
+	// 	sl: "33",
+	// 	Name: "Dayananda Bindhani",
+	// },
+	// {
+	// 	sl: "34",
+	// 	Name: "Rishabh Kumar",
+	// },
+	// {
+	// 	sl: "35",
+	// 	Name: "Abhinand Anilkumar",
+	// },
+	// {
+	// 	sl: "36",
+	// 	Name: "Aniket Shahi",
+	// },
+	// {
+	// 	sl: "37",
+	// 	Name: "prashant hiremath",
+	// },
+	// {
+	// 	sl: "38",
+	// 	Name: "Divyanshu Kashyap",
+	// },
+	// {
+	// 	sl: "39",
+	// 	Name: "Akash",
+	// },
+	// {
+	// 	sl: "40",
+	// 	Name: "Abhay Gupta",
+	// },
+	// {
+	// 	sl: "41",
+	// 	Name: "Vijay hiremath",
+	// },
+	// {
+	// 	sl: "42",
+	// 	Name: "Satyam Krishna",
+	// },
+	// {
+	// 	sl: "43",
+	// 	Name: "Madhav Jadav",
+	// },
+	// {
+	// 	sl: "44",
+	// 	Name: "Aditya Bahl",
+	// },
+	// {
+	// 	sl: "45",
+	// 	Name: "Ritesh Kumar Goel",
+	// },
+	// {
+	// 	sl: "46",
+	// 	Name: "Netransh Adhoj",
+	// },
+	// {
+	// 	sl: "47",
+	// 	Name: "Anushka Ata",
+	// },
+	// {
+	// 	sl: "48",
+	// 	Name: "Rusi",
+	// },
+	// {
+	// 	sl: "49",
+	// 	Name: "Khushi Shrivastava",
+	// },
+	// {
+	// 	sl: "50",
+	// 	Name: "Shreyash",
+	// },
+	// {
+	// 	sl: "51",
+	// 	Name: "Dhruv Suri",
+	// },
+	// {
+	// 	sl: "52",
+	// 	Name: "VIVEK KUMAR",
+	// },
+	// {
+	// 	sl: "53",
+	// 	Name: "Kushagra Singh",
+	// },
+	// {
+	// 	sl: "54",
+	// 	Name: "Aditi Nagpal",
+	// },
+	// {
+	// 	sl: "55",
+	// 	Name: "Sourabh Roj",
+	// },
+	// {
+	// 	sl: "56",
+	// 	Name: "Alok Danve",
+	// },
+	// {
+	// 	sl: "57",
+	// 	Name: "Bhawana",
+	// },
+	// {
+	// 	sl: "58",
+	// 	Name: "Rakhi Kumari",
+	// },
+	// {
+	// 	sl: "59",
+	// 	Name: "Suresh",
+	// },
+	// {
+	// 	sl: "60",
+	// 	Name: "Shreya",
+	// },
+	// {
+	// 	sl: "61",
+	// 	Name: "Vivek Kumar",
+	// },
+	// {
+	// 	sl: "62",
+	// 	Name: "Tisya Kaushik",
+	// },
+	// {
+	// 	sl: "63",
+	// 	Name: "Vivek kr Jha",
+	// },
+	// {
+	// 	sl: "64",
+	// 	Name: "Aakash Kharb",
+	// },
+	// {
+	// 	sl: "65",
+	// 	Name: "Roushan Kumar Singh",
+	// },
+	// {
+	// 	sl: "66",
+	// 	Name: "Priyanshi Pawar",
+	// },
+	// {
+	// 	sl: "67",
+	// 	Name: "Vaibhav Tripathi",
+	// },
+	// {
+	// 	sl: "68",
+	// 	Name: "Susovan Pati",
+	// },
+	// {
+	// 	sl: "69",
+	// 	Name: "Vijayakumar Hiremath",
+	// },
+	// {
+	// 	sl: "70",
+	// 	Name: "Kushagra",
+	// },
+	// {
+	// 	sl: "71",
+	// 	Name: "Hemanth Prabhu",
+	// },
+	// {
+	// 	sl: "72",
+	// 	Name: "Anand Kumar",
+	// },
+	// {
+	// 	sl: "73",
+	// 	Name: "Arpita Parihar",
+	// },
+	// {
+	// 	sl: "74",
+	// 	Name: "Satvik bhagat",
+	// },
+	// {
+	// 	sl: "75",
+	// 	Name: "Aditya",
+	// },
+	// {
+	// 	sl: "76",
+	// 	Name: "Digvijay Singh",
+	// },
+	// {
+	// 	sl: "77",
+	// 	Name: "Vikash",
+	// },
+	// {
+	// 	sl: "78",
+	// 	Name: "Pradeep",
+	// },
+	// {
+	// 	sl: "79",
+	// 	Name: "Aryan Sadh",
+	// },
+	// {
+	// 	sl: "80",
+	// 	Name: "Kavi",
+	// },
+	// {
+	// 	sl: "81",
+	// 	Name: "Anand Kumar",
+	// },
+	// {
+	// 	sl: "82",
+	// 	Name: "Jaideep",
+	// },
+	// {
+	// 	sl: "83",
+	// 	Name: "PRIYANSHU JENA",
+	// },
+	// {
+	// 	sl: "84",
+	// 	Name: "Prince Kumar",
+	// },
+	// {
+	// 	sl: "85",
+	// 	Name: "VISHNULAL N",
+	// },
+	// {
+	// 	sl: "86",
+	// 	Name: "Abhilasha Kumari",
+	// },
+	// {
+	// 	sl: "87",
+	// 	Name: "Kajal Meena",
+	// },
+	// {
+	// 	sl: "88",
+	// 	Name: "Manu krishnan k",
+	// },
+	// {
+	// 	sl: "89",
+	// 	Name: "Rachna Singh",
+	// },
+	// {
+	// 	sl: "90",
+	// 	Name: "Sneha Das",
+	// },
+	// {
+	// 	sl: "91",
+	// 	Name: "Aditya Dahiya",
+	// },
 ];
