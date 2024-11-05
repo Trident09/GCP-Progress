f03c8484ee11b68a79afeb35a4dc6f4a02ae54a7
Kushagra Singh <kushagra.singh1501@gmail.com>
2024-11-05 21:42:31 +0530

    update header link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
 	return (
 		<nav className="fixed navbar navbar-expand-lg navbar-light bg-light w-full z-50">
 			<div className="container-fluid">
-				<a href="https://gdsc.community.dev/national-forensic-sciences-university-delhi/">
+				<a href="https://gdg.community.dev/gdg-on-campus-national-forensic-sciences-university-delhi-india/">
 					<img
 						className="d-inline-block align-text-top"
 						src="https://raw.githubusercontent.com/Trident09/gcp-progress/main/src/assets/gdg-nfsu-logo.png"
